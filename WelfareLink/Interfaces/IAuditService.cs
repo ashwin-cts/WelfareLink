ff4a138f842480dcb6b13a245c0b56c691a83a92
jayeshsaindane54 <jayeshjitendra.saindane@cognizant.com>
2026-03-27 10:59:28 +0530

    Update IAuditService.cs

--- WelfareLink/Interfaces/IAuditService.cs
+++ WelfareLink/Interfaces/IAuditService.cs
@@ -1,5 +1,8 @@
 namespace WelfareLink.Interfaces;
 
 public interface IAuditService
-{
+{    Task<IEnumerable<ComplainceRecord>> GetAllRecordsAsync();
+    Task<ComplainceRecord> GetRecordByIdAsync(string complianceId);
+    Task<bool> CreateRecordAsync(ComplainceRecord record);
+    Task<IEnumerable<ComplainceRecord>> GetRecordsByEntityAsync(string entityId);
 }
