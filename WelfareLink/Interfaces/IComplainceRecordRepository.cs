5af490ade2cbdfc5675f0e8e8959e0fd1cfb9742
jayeshsaindane54 <jayeshjitendra.saindane@cognizant.com>
2026-03-27 11:18:21 +0530

    Update IComplainceRecordRepository.cs

--- WelfareLink/Interfaces/IComplainceRecordRepository.cs
+++ WelfareLink/Interfaces/IComplainceRecordRepository.cs
@@ -2,4 +2,8 @@ namespace WelfareLink.Interfaces;
 
 public interface IComplainceRecordRepository
 {
+   Task<IEnumerable<ComplainceRecord>> GetAllRecordsAsync();
+ Task<ComplainceRecord> GetRecordByIdAsync(string complianceId);
+ Task<bool> CreateRecordAsync(ComplainceRecord record);
+ Task<IEnumerable<ComplainceRecord>> GetRecordsByEntityAsync(string entityId);
 }
