1d6a71246185a4719c9b71bf7295888caa9ae096
jayeshsaindane54 <jayeshjitendra.saindane@cognizant.com>
2026-04-01 09:04:22 +0530

    Update ComplainceRecord.cs

--- WelfareLink/Models/ComplainceRecord.cs
+++ WelfareLink/Models/ComplainceRecord.cs
@@ -1,6 +1,36 @@
-﻿namespace WelfareLink.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WelfareLink.Models
 {
-    public class ComplainceRecord
+    public class ComplianceRecord
     {
+        [Key]
+        [StringLength(36)]
+        public string ComplianceID { get; set; }
+       // [cite: 100]
+
+        [Required(ErrorMessage = "Entity ID is required")]
+        [StringLength(36)]
+        public string EntityID { get; set; }
+      //  [cite: 100]
+
+        [Required(ErrorMessage = "Type is required")]
+        [StringLength(30)]
+        public string Type { get; set; } // Application, Benefit, or Program [cite: 73, 100]
+
+        [Required(ErrorMessage = "Result is required")]
+        [StringLength(30)]
+        public string Result { get; set; }
+      //  [cite: 100]
+
+        [Required(ErrorMessage = "Date is required")]
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+      //  [cite: 100]
+
+        [StringLength(2000)]
+        public string? Notes { get; set; }
+       // [cite: 100]
     }
 }
