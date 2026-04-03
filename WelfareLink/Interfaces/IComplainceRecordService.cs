68fea720ceab9ebd482bb70f9d0d400169b1f0a9
jayeshsaindane54 <jayeshjitendra.saindane@cognizant.com>
2026-04-03 08:45:37 +0530

    Update IComplainceRecordService.cs

--- WelfareLink/Interfaces/IComplainceRecordService.cs
+++ WelfareLink/Interfaces/IComplainceRecordService.cs
@@ -1,5 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WelfareLink.Models;
+
 namespace WelfareLink.Interfaces;
 
 public interface IComplainceRecordService
 {
+    Task<IEnumerable<ComplainceRecord>> GetAllAsync();
+
+    Task<ComplainceRecord?> GetByIdAsync(string id);
+
+    Task CreateAsync(ComplainceRecord record);
+
+    Task UpdateAsync(ComplainceRecord record);
+
+    Task DeleteAsync(string id);
 }
