4b295d210568a617060f77eabc0196ab10082c18
jayeshsaindane54 <jayeshjitendra.saindane@cognizant.com>
2026-04-03 08:44:20 +0530

    Update IAuditService.cs

--- WelfareLink/Interfaces/IAuditService.cs
+++ WelfareLink/Interfaces/IAuditService.cs
@@ -1,5 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WelfareLink.Models;
+
 namespace WelfareLink.Interfaces;
 
 public interface IAuditService
 {
+    Task<IEnumerable<Audit>> GetAllAsync();
+
+    Task<Audit?> GetByIdAsync(string id);
+
+    Task CreateAsync(Audit audit);
+
+    Task UpdateAsync(Audit audit);
+
+    Task DeleteAsync(string id);
 }
