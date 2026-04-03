0647a2b4e788206838896e424340e276e82f3637
jayeshsaindane54 <jayeshjitendra.saindane@cognizant.com>
2026-04-03 08:42:39 +0530

    Update HomeController.cs

--- WelfareLink/Controllers/HomeController.cs
+++ WelfareLink/Controllers/HomeController.cs
@@ -1,14 +1,36 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using WelfareLink.Models;
+using WelfareLink.Interfaces;
+using WelfareLink.ViewModels;
 
 namespace WelfareLink.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IAuditService _auditService;
+        private readonly IComplainceRecordService _complService;
+
+        public HomeController(IAuditService auditService, IComplainceRecordService complService)
         {
-            return View();
+            _auditService = auditService;
+            _complService = complService;
+        }
+
+        // Show audits and compliance records on the home page
+        public async Task<IActionResult> Index()
+        {
+            var audits = await _auditService.GetAllAsync();
+            var records = await _complService.GetAllAsync();
+
+            var model = new DashboardViewModel
+            {
+                Audits = audits,
+                ComplainceRecords = records
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
