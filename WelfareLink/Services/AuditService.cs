48d3b14d47a1b9ed25553bf99bf9aac464029685
jayeshsaindane54 <jayeshjitendra.saindane@cognizant.com>
2026-04-03 08:48:45 +0530

    Update AuditService.cs

--- WelfareLink/Services/AuditService.cs
+++ WelfareLink/Services/AuditService.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using WelfareLink.Interfaces;
+using WelfareLink.Models;
 
 namespace WelfareLink.Services;
 
 public class AuditService : IAuditService
 {
+    private readonly IAuditRepository _repo;
+
+    public AuditService(IAuditRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public Task<IEnumerable<Audit>> GetAllAsync() => _repo.GetAllAsync();
+
+    public Task<Audit?> GetByIdAsync(string id) => _repo.GetByIdAsync(id);
+
+    public Task CreateAsync(Audit audit) => _repo.AddAsync(audit);
+
+    public Task UpdateAsync(Audit audit) => _repo.UpdateAsync(audit);
+
+    public Task DeleteAsync(string id) => _repo.DeleteAsync(id);
 }
